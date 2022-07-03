a86c2b90f0d3d0df0e0bc3c749b7630df230442b
Jason <jdclaeys@outlook.com>
2022-07-03 14:20:07 -0500

    add comments

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+// Require packages for this file
 const express = require('express')
 
+// Initialize express
 const app = express()
 
+// Routes
 app.get('/', (req, res) => {
   res.send('INDEX PAGE')
 })
 
 const PORT = process.env.PORT || 3000
 
+// Set Express server to listen to port 3000
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}`)
 })
