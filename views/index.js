52436f8f82c348d8f3b37a5695286aec7ad13d46
Jason <jdclaeys@outlook.com>
2022-07-03 18:37:01 -0500

    add Materialize CSS

--- views/index.js
+++ views/index.js
@@ -2,9 +2,17 @@ const indexPage = () => {
   return `
     <!DOCTYPE html>
     <html>
-      <head></head>
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <title>My Business</title>
+      </head>
       <body>
         <h1>Index Page</h1>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
       </body>
     </html>
   `
