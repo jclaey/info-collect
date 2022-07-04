15becd985826dd24c237d9b248318177df707395
Jason <jdclaeys@outlook.com>
2022-07-03 21:08:43 -0500

    add form to index page

--- views/index.js
+++ views/index.js
@@ -18,6 +18,17 @@ const indexPage = () => {
         </header>
         <main class="container">
           <h1>My Business</h1>
+          <form action="/" method="POST">
+            <div class="input-field">
+              <input type="text" id="firstName" name="firstName" placeholder="Enter first name...">
+              <label for="firstName">First name</label>
+            </div>
+            <div class="input-field">
+              <input type="text" id="lastName" name="lastName" placeholder="Enter last name...">
+              <label for="lastName">Last name</label>
+            </div>
+            <button type="submit" class="btn-large">Submit</button>
+          </form>
         </main>
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
