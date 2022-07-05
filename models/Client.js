207ba7c5f057f173609c6c719ab879dadb8b3f55
Jason <jdclaeys@outlook.com>
2022-07-05 16:50:07 -0500

    add email property to client model

---
+++ models/Client.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ClientSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true
+  },
+  addressOne: {
+    type: String,
+    required: true
+  },
+  addressTwo: String,
+  insuranceCompany: {
+    type: String,
+    required: true
+  },
+  vehicleYear: {
+    type: String,
+    required: true
+  },
+  vehicleMake: {
+    type: String,
+    required: true
+  },
+  vehicleModel: {
+    type: String,
+    required: true
+  },
+  policyNumber: {
+    type: String,
+    required: true
+  },
+  expirationDate: {
+    type: Date
+  },
+  driversLicense: {
+    path: String,
+    filename: String,
+    required: true
+  },
+  policyCard: {
+    path: String,
+    filename: String,
+    required: true
+  },
+  submittedAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Client', ClientSchema)
