68c600a5b61d720bec278a30103524f1e027af9f
Jason <jdclaeys@outlook.com>
2022-08-01 19:50:38 -0500

    add errors to login page

--- views/admin/login.js
+++ views/admin/login.js
@@ -1,11 +1,21 @@
 const layout = require('../layout')
+const getErrors = require('../helpers')
 
-const loginPage = ({}) => {
+const loginPage = ({ errors }) => {
   return layout({ template: 
     `
       <main class="container">
         <h1>Sign In</h1>
         <div class="row">
+          ${errors ? 
+            `
+              <div class="col s12" style="border: 1px solid #9E9E9E; margin-bottom: 2rem">
+                <div style="margin-bottom: 1rem">
+                  ${getErrors(errors)}
+                </div>
+              </div>
+            ` 
+          : ''}
           <div class="form-container col s12">
             <form action="/admin/dashboard" method="POST">
               <div class="row">
