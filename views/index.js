c0696fee2e16710288c973582f1e77a9037b6958
Jason <jdclaeys@outlook.com>
2022-08-01 19:49:45 -0500

    persist form data across re-renders

--- views/index.js
+++ views/index.js
@@ -1,10 +1,10 @@
 const layout = require('./layout')
 const { getErrors } = require('./helpers')
 
-const indexPage = ({ errors }) => {
+const indexPage = ({ errors, values = {} }) => {
   return layout({ template: `
     <main class="container">
-      <h1>Some Business</h1>
+      <h1>Some Text</h1>
       <div class="row">
         ${errors ? 
           `
@@ -19,61 +19,61 @@ const indexPage = ({ errors }) => {
           <form action="/" method="POST">
             <div class="row">
               <div class="input-field col s6">
-                <input type="text" id="firstName" name="firstName">
+                <input type="text" id="firstName" name="firstName" value="${errors && values.firstName && values.firstName !== '' ? values.firstName : ''}">
                 <label for="firstName">*First name</label>
               </div>
               <div class="input-field col s6">
-                <input type="text" id="lastName" name="lastName">
+                <input type="text" id="lastName" name="lastName" value="${errors && values.lastName && values.lastName !== '' ? values.lastName : ''}">
                 <label for="lastName">*Last name</label>
               </div>
             </div>
             <div class="row">
               <div class="input-field col s12">
-                <input type="text" id="email" name="email">
+                <input type="text" id="email" name="email" value="${errors && values.email && values.email !== '' ? values.email : ''}">
                 <label for="email">*Email address</label>
               </div>
             </div>
             <div class="row">
               <div class="input-field col s12">
-                <input type="text" id="phoneNumber" name="phoneNumber">
+                <input type="text" id="phoneNumber" name="phoneNumber" value="${errors && values.phoneNumber && values.phoneNumber !== '' ? values.phoneNumber : ''}">
                 <label for="phoneNumber">*Phone number</label>
               </div>
             </div>
             <div class="row">
               <div class="input-field col s12">
-                <input type="text" id="addressLineOne" name="addressLineOne">
+                <input type="text" id="addressLineOne" name="addressLineOne" value="${errors && values.addressLineOne && values.addressLineOne !== '' ? values.addressLineOne : ''}">
                 <label for="addressLineOne">*Address</label>
               </div>
             </div>
             <div class="row">
               <div class="input-field col s12">
-                <input type="text" id="addressLineTwo" name="addressLineTwo">
+                <input type="text" id="addressLineTwo" name="addressLineTwo" value="${errors && values.addressLineTwo && values.addressLineTwo !== '' ? values.addressLineTwo : ''}">
                 <label for="addressLineTwo">Address Line 2</label>
               </div>
             </div>
             <div class="row">
               <div class="input-field col s12">
-                <input type="text" id="insuranceCompany" name="insuranceCompany">
+                <input type="text" id="insuranceCompany" name="insuranceCompany" value="${errors && values.insuranceCompany && values.insuranceCompany !== '' ? values.insuranceCompany : ''}">
                 <label for="insuranceCompany">*Insurance company</label>
               </div>
             </div>
             <div class="row">
               <div class="input-field col s4">
-                <input type="text" id="vehicleYear" name="vehicleYear">
+                <input type="text" id="vehicleYear" name="vehicleYear" value="${errors && values.vehicleYear && values.vehicleYear !== '' ? values.vehicleYear : ''}">
                 <label for="vehicleYear">*Vehicle year</label>
               </div>
               <div class="input-field col s4">
-                <input type="text" id="vehicleMake" name="vehicleMake">
+                <input type="text" id="vehicleMake" name="vehicleMake" value="${errors && values.vehicleMake && values.vehicleMake !== '' ? values.vehicleMake : ''}">
                 <label for="vehicleMake">*Vehicle make</label>
               </div>
               <div class="input-field col s4">
-                <input type="text" id="vehicleModel" name="vehicleModel">
+                <input type="text" id="vehicleModel" name="vehicleModel" value="${errors && values.vehicleModel && values.vehicleModel !== '' ? values.vehicleModel : ''}">
                 <label for="vehicleModel">*Vehicle model</label>
               </div>
             </div>
             <div class="row">
               <div class="input-field col s6">
-                <input type="text" id="policyNumber" name="policyNumber">
+                <input type="text" id="policyNumber" name="policyNumber" value="${errors && values.policyNumber && values.policyNumber !== '' ? values.policyNumber : ''}">
                 <label for="policyNumber">*Policy number</label>
               </div>
               <div class="input-field col s6">
