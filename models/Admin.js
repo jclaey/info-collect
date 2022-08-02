5ad5bda7d0eb911a62b657168764356ab0ac034b
Jason <jdclaeys@outlook.com>
2022-08-01 19:58:12 -0500

    add compare passwords logic to admin schema

--- models/Admin.js
+++ models/Admin.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto')
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
@@ -5,5 +6,13 @@ const AdminSchema = new Schema({
   email: String,
   password: String
 })
+
+AdminSchema.methods.comparePasswords = function(suppliedPassword) {
+  const [hashed, salt] = this.password.split('.')
+
+  const hashedSupplied = crypto.createHash('sha256').update(suppliedPassword + salt).digest('hex')
+
+  return hashedSupplied === hashed
+}
 
 module.exports = mongoose.model('Admin', AdminSchema)
