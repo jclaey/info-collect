9db98526ed28afd7569dd1f0a60cfa1598ff081b
Jason <jdclaeys@outlook.com>
2022-08-03 20:59:53 -0500

    add admin log in functionality and add client data to dashboard route

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -1,11 +1,41 @@
 const { validationResult } = require('express-validator')
 const loginPage = require('../../views/admin/login')
+const adminDashboardPage = require('../../views/admin/dashboard')
+const Admin = require('../../models/Admin')
+const Client = require('../../models/Client')
 
 module.exports = {
+  getLogout(req, res, next) {
+    req.session = null
+    res.redirect('/admin/login')
+  },
   getLogin(req, res, next) {
     res.send(loginPage({}))
   },
-  postLogin(req, res, next) {
-    
+  async postLogin(req, res, next) {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+      return res.send(loginPage({ errors }))
+    }
+
+    const { email } = req.body
+
+    const admin = await Admin.findOne({ email })
+
+    req.session.userId = String(admin._id)
+
+    res.redirect('/admin/dashboard')
+  },
+  async getClients(req, res, next) {
+    const clients = await Client.find({})
+
+    if (!clients) {
+      // return res.send(errorPage(msg))
+      res.status(404)
+      throw new Error('Could not get clients')
+    }
+
+    res.send(adminDashboardPage({ clients }, req))
   }
 }
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const asyncHandler = require('../../middleware/async')
 const {
   requireEmailExists,
   requireValidPasswordForUser
 } = require('../validators')
 const {
+  getLogout,
   getLogin,
-  postLogin
+  postLogin,
+  getClients
 } = require('../../controllers/admin')
 
 router.route('/login')
@@ -15,5 +18,9 @@ router.route('/login')
     requireEmailExists,
     requireValidPasswordForUser
   ], postLogin)
+
+router.get('/logout', getLogout)
+
+router.get('/dashboard', asyncHandler(getClients))
 
 module.exports = router
