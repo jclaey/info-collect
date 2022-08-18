7f87d2f60383ce5ae6795448456145f844b64e43
Jason <jdclaeys@outlook.com>
2022-08-17 21:15:43 -0500

    add footer

--- views/layout.js
+++ views/layout.js
@@ -1,4 +1,5 @@
 const header = require('./partials/header')
+const footer = require('./partials/footer')
 
 const layout = ({ template }, req) => {
   return `
@@ -18,7 +19,13 @@ const layout = ({ template }, req) => {
             ${header(req)}
           </header>
           ${template}
+          <footer class="page-footer">
+            ${footer()}
+          </footer>
+
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+          <script src="/javascript/side-nav.js"></script>
         </body>
       </html>
   `
---
+++ views/partials/footer.js
@@ -0,0 +1,11 @@
+const footer = () => {
+  return `
+    <div id="footer-div">
+      <p>
+        Built by <a href="https://claeysdev.com" id="built-by" target="_blank">ClaeysDev</a>
+      </p>
+    </div>
+  `
+}
+
+module.exports = footer
