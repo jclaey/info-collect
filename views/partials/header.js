8e112920b2fe232678610a6eef3acb7a9653f1bb
Jason <jdclaeys@outlook.com>
2022-08-17 21:14:47 -0500

    add claim link to header

--- views/partials/header.js
+++ views/partials/header.js
@@ -2,8 +2,10 @@ const header = (req = null) => {
   return `
     <nav>
       <div class="nav-wrapper">
-        <a href="/" class="brand-logo">My Business</a>
+        <a href="/" class="brand-logo">V. Raju</a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="/claim-form">File A Claim</a></li>
           ${req && req.session && req.session.userId
             ? '<li><a href="/admin/logout">Sign Out</a></li>'
             : ''
@@ -11,6 +13,14 @@ const header = (req = null) => {
         </ul>
       </div>
     </nav>
+
+    <ul class="sidenav" id="mobile-demo">
+      <li><a href="/claim-form">File A Claim</a></li>
+      ${req && req.session && req.session.userId
+        ? '<li><a href="/admin/logout">Sign Out</a></li>'
+        : ''
+      }
+    </ul>
   `
 }
 
