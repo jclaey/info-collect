e1fe381c9f9d3ba2961357ab98b6cdb0284f9bfb
Jason <jdclaeys@outlook.com>
2022-08-19 16:46:38 -0500

    add validators to restrict input lengths

--- routes/validators.js
+++ routes/validators.js
@@ -2,42 +2,51 @@ const { body } = require('express-validator')
 const Admin = require('../models/Admin')
 
 module.exports = {
-  validateFirstName: body('firstName', 'Please provide a first name')
+  validateFirstName: body('firstName')
     .trim()
-    .isLength({ min: 1 }),
+    .isLength({ min: 1, max: 30 })
+    .withMessage('First name must be between 1 and 30 characters'),
   validateLastName: body('lastName', 'Please provide a last name')
     .trim()
-    .isLength({ min: 1 }),
-  validateEmail: body('email', 'Please provide a valid email')
+    .isLength({ min: 1, max: 30 }),
+  validateEmail: body('email')
     .trim()
     .normalizeEmail()
-    .isEmail(),
+    .isEmail()
+    .withMessage('First name must be between 1 and 30 characters'),
   validatePhoneNumber: body('phoneNumber')
     .trim()
-    .isLength({ min: 1 })
-    .withMessage('Please provide a phone number')
+    .isLength({ min: 10, max: 10 })
+    .withMessage('Phone number must be 10 digits')
     .isMobilePhone()
     .withMessage('Please provide a valid phone number'),
-  validateAddressOne: body('addressOne', 'Please provide an address')
+  validateAddressOne: body('addressOne')
     .trim()
-    .isLength({ min: 1 }),
+    .isLength({ min: 1, max: 50 })
+    .withMessage('Address must be between 1 and 50 characters'),
   validateAddressTwo: body('addressTwo')
-    .trim(),
-  validateInsuranceCompany: body('insuranceCompany', 'Please provide an insurance company')
     .trim()
-    .isLength({ min: 1 }),
+    .isLength({ max: 50 })
+    .withMessage('Address cannot be longer than 50 characters'),
+  validateInsuranceCompany: body('insuranceCompany')
+    .trim()
+    .isLength({ min: 1, max: 30 })
+    .withMessage('Insurance company must be between 1 and 30 characters'),
   validateVehicleYear: body('vehicleYear', 'Please provide a vehicle year')
     .trim()
     .isLength({ min: 4, max: 4 }),
   validateVehicleMake: body('vehicleMake', 'Please provide a vehicle make')
     .trim()
-    .isLength({ min: 1 }),
-  validateVehicleModel: body('vehicleModel', 'Please provide a vehicle model')
+    .isLength({ min: 1, max: 30 })
+    .withMessage('Vehicle make must be between 1 and 30 characters'),
+  validateVehicleModel: body('vehicleModel')
     .trim()
-    .isLength({ min: 1 }),
-  validatePolicyNumber: body('policyNumber', 'Please provide a policy number')
+    .isLength({ min: 1, max: 30 })
+    .withMessage('Vehicle model must be between 1 and 30 characters'),
+  validatePolicyNumber: body('policyNumber')
     .trim()
-    .isLength({ min: 1 }),
+    .isLength({ min: 1, max: 30 })
+    .withMessage('Policy number must be between 1 and 30 characters'),
   validateExpirationDate: body('expirationDate', 'Please provide an expiration date')
     .trim()
     .isDate({ format: 'MM/DD/YYYY' }),
@@ -46,6 +55,8 @@ module.exports = {
     .normalizeEmail()
     .isEmail()
     .withMessage('Must provide a valid email')
+    .isLength({ min: 1, max: 30 })
+    .withMessage('Email must be between 1 and 30 characters')
     .custom(async email => {
       const admin = await Admin.findOne({ email })
 
@@ -55,6 +66,8 @@ module.exports = {
     }),
   requireValidPasswordForUser: body('password')
     .trim()
+    .isLength({ max: 30 })
+    .withMessage('Password cannot be longer than 30 characters')
     .custom(async (password, { req }) => {
       const admin = await Admin.findOne({ email: req.body.email })
 
